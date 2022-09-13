7428ff0f794df8e0dfb0fd066d4b4a0bac874e31
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2022-09-13 16:36:50 +0700

    New aliases

--- Backups/aliases.zsh
+++ Backups/aliases.zsh
@@ -1,4 +1,20 @@
 alias gh="gh.exe"
-alias ZSH_CUSTOM="$ZSH/custom"
 alias prog="cd /mnt/c/Programming"
 alias del="rm -rf"
+alias configAliases="
+pushd
+cd $ZSH/custom
+code aliases.zsh
+popd
+popd
+"
+alias configZsh="
+pushd
+code ~/.zshrc
+popd
+"
+alias configP10k="
+pushd
+code ~/.p10k.zsh
+popd
+"
